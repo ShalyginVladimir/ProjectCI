7e5036fb9d6fb68432a35399ad2839863101ba7c
Vladimir Shalygin <vova.shalygin.88@mail.ru>
2023-06-13 21:45:39 +0300

    new test

--- src/test/java/org/example/ProjectCI/services/StatisticsServiceTest.java
+++ src/test/java/org/example/ProjectCI/services/StatisticsServiceTest.java
@@ -8,11 +8,23 @@ import static org.junit.jupiter.api.Assertions.*;
 public class StatisticsServiceTest {
 
     @Test
-    void findMax() {
+    void findMaxStandard() {
         StatisticsService service = new StatisticsService();
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxNew() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 70, 8, 6, 11, 11, 12};
+        long expected = 70;
 
         long actual = service.findMax(incomesInBillions);
 
